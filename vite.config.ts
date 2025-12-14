1874e06d418e8de907bcc81d52dc73dd6fc5fd6d
uglednimomak <ante93366@gmail.com>
2025-12-14 19:31:35 +0000

    Update alias path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -16,7 +19,7 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': path.resolve(__dirname, 'src'),
         }
       }
     };
